acb0ff96bd6ce8c1c988636df299379b789e9997
Laem23 <107215736+Laem23@users.noreply.github.com>
2022-12-10 18:53:30 +0700

    เพิ่มชื่อผู้พัฒนา

--- hello_githup.py
+++ hello_githup.py
@@ -1,6 +1,3 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Hello Githup')
-Hello Githup
->>> print('Hello World')
-Hello World
+print('Hello Githut')
+
+print('by Leam Charoenchai')
